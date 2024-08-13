e480cfc7e327ba7494c25a2e795fa3e046822b9d
Felipe Valencia <pipeedev@gmail.com>
2024-08-12 20:39:09 -0400

    chore: wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,24 @@
+local wezterm = require("wezterm")
+
+config = wezterm.config_builder()
+
+config = {
+	automatically_reload_config = true,
+	enable_tab_bar = false,
+	-- window_decorations = "RESIZE",
+	default_cursor_style = "BlinkingBar",
+	font = wezterm.font("FiraCode Nerd Font Mono", { weight = "Regular" }),
+	font_size = 14.0,
+	color_scheme = "Github Dark (Gogh)",
+	window_padding = {
+		left = 3,
+		right = 3,
+		top = 3,
+		bottom = 0,
+	},
+}
+
+config.window_background_opacity = 0.95
+config.macos_window_background_blur = 10
+
+return config
